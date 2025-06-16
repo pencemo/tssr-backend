2ebeb03115bb33a6a6329780c62421bc08551e6c
SADHIN <muhammedsadhin9745@gmail.com>
2025-06-16 16:42:37 +0530

    set up api bug

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,14 +1,16 @@
 import express from "express";
-import {  getAllStudentsDownloadForAdmin, getAllStudentsSample, getOneStudent, getStudentsForDl, getStudyCenterStudents } from "../controllers/studentController.js";
+import {  getAllStudentsDownloadForAdmin, getOneStudent, getStudentsForDl, getStudyCenterStudents } from "../controllers/studentController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 import { reportDownloadAccess } from "../middlewares/reportsDownloadMiddleware.js";
 const router = express.Router();
 
 
-router.get("/getAllStudents", isAuthenticated, getAllStudentsSample);
+router.get("/getAllStudents", isAuthenticated, getStudyCenterStudents);
 router.get("/getOneStudent", isAuthenticated, getOneStudent);
 router.post("/getStudentsForDl", isAuthenticated, reportDownloadAccess, getStudentsForDl);
 router.post("/getAllStudentForAdmin", getAllStudentsDownloadForAdmin);
 
 export default router;
+
+
 
