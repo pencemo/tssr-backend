1919212bd59e6acba153ddf2d958f535f4f7251c
unknown <applenetdesign@gmail.com>
2025-08-01 18:23:33 +0530

    update origns

--- server.js
+++ server.js
@@ -53,13 +53,15 @@ const allowedOrigins = [
 // }))
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+    console.log("Request origin:", origin);
     
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      console.log("CORS Blocked Origin:", origin);
+      return callback(new Error('Not allowed by CORS'));
     }
   },
   credentials: true,
