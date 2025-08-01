3cd7934b7e0c34457169fb685cf1ec47df99b3e9
unknown <applenetdesign@gmail.com>
2025-08-01 18:10:56 +0530

    edit cors

--- server.js
+++ server.js
@@ -46,11 +46,25 @@ const allowedOrigins = [
   'http://localhost:5173'
 ];
 
+// app.use(cors({
+//       origin: allowedOrigins,
+//       credentials: true,
+//       methods: ["GET", "POST","PUT","DELETE"],
+// }))
 app.use(cors({
-      origin: allowedOrigins,
-      credentials: true,
-      methods: ["GET", "POST","PUT","DELETE"],
-}))
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 app.use(morgan("dev"));
 
 // Routes
